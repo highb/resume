c653437f5c55cd1c1ffdd7f14eb07e39c53c4b53
Brandon High <759848+highb@users.noreply.github.com>
2025-08-31 12:40:49 -0700

    build extended resume

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 docker build -t sb2nov/latex .
 docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex brandon_high_resume.tex
+docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex brandon_high_resume_extended.tex
